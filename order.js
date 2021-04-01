674f46afedb2a4187fed57e3538bcfa80f5b14d3
Somin Kim <80324634+thals1214@users.noreply.github.com>
2021-04-01 22:02:48 +0900

    Add files via upload

---
+++ order.js
@@ -0,0 +1,11 @@
+const orders = {
+    orderNum : 1,
+    orderId: "efubFront",
+    name: "이펍",
+    address: " 서울특별시 서대문구 이화여대길52"
+};
+
+for(let key in orders){
+    console.log(`${key}: ${orders[key]}`);
+}
+
